17a4ffe968694c2779eb7cec468dcd5219a9b37d
kun <huangzhongkun@douban.com>
2013-07-20 15:07:15 +0800

    add files

---
+++ js/typing.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+
+	$(document).bind('keyup', onKeyUp);
+	
+	function onKeyUp(evt) {
+		$('#current_input').html(String.fromCharCode(evt.keyCode));
+				
+		clearText();
+		
+		var txt = $('#code_txt').html();
+		
+		
+		console.log('code_txt', txt);
+	}
+	
+	function clearText() {
+	    var divMain = $('#main_div');
+	    var spans =$('#main_div > span');
+	    while (spans.length > 0) {
+	        divMain.removeChild(spans[0]);
+	    }
+	}
+
+});
